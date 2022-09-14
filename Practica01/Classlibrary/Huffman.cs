66bc2fb9d91371416aeed68fd4d7bf5afba57753
DulceGr <fmarigd@gmail.com>
2022-09-13 19:05:05 -0600

    corrección de bug de archivo, lectura y algoritmo

---
+++ Practica01/Classlibrary/Huffman.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classlibrary
+{
+    [Serializable()]
+    public class Huffman
+    {
+        private List<HuffmanNode> nodes = new List<HuffmanNode>();
+        public HuffmanNode Root { get; set; }
+        public Dictionary<char, int> Frequencies = new Dictionary<char, int>();
+
+        public void Build(string source)
+        {
+            for (int i = 0; i < source.Length; i++)
+            {
+                if (!Frequencies.ContainsKey(source[i]))
+                {
+                    Frequencies.Add(source[i], 0);
+                }
+
+                Frequencies[source[i]]++;
+            }
+
+            foreach (KeyValuePair<char, int> symbol in Frequencies)
+            {
+                nodes.Add(new HuffmanNode() { Character = symbol.Key, Frequency = symbol.Value });
+            }
+
+            while (nodes.Count > 1)
+            {
+                List<HuffmanNode> orderedNodes = nodes.OrderBy(node => node.Frequency).ToList<HuffmanNode>();
+
+                if (orderedNodes.Count >= 2)
+                {
+                    // Take first two items
+                    List<HuffmanNode> taken = orderedNodes.Take(2).ToList<HuffmanNode>();
+
+                    // Create a parent node by combining the frequencies
+                    HuffmanNode parent = new HuffmanNode()
+                    {
+                        Character = '*',
+                        Frequency = taken[0].Frequency + taken[1].Frequency,
+                        LeftNode = taken[0],
+                        RightNode = taken[1]
+                    };
+
+                    nodes.Remove(taken[0]);
+                    nodes.Remove(taken[1]);
+                    nodes.Add(parent);
+                }
+
+                this.Root = nodes.FirstOrDefault();
+
+            }
+
+        }
+
+        public BitArray Encode(string source)
+        {
+            List<bool> encodedSource = new List<bool>();
+
+            for (int i = 0; i < source.Length; i++)
+            {
+                List<bool> encodedSymbol = this.Root.Traverse(source[i], new List<bool>());
+                encodedSource.AddRange(encodedSymbol);
+            }
+
+            BitArray bits = new BitArray(encodedSource.ToArray());
+
+            return bits;
+        }
+
+        public string Decode(BitArray bits)
+        {
+            HuffmanNode current = this.Root;
+            string decoded = "";
+
+            foreach (bool bit in bits)
+            {
+                if (bit)
+                {
+                    if (current.RightNode != null)
+                    {
+                        current = current.RightNode;
+                    }
+                }
+                else
+                {
+                    if (current.LeftNode != null)
+                    {
+                        current = current.LeftNode;
+                    }
+                }
+
+                if (IsLeaf(current))
+                {
+                    decoded += current.Character;
+                    current = this.Root;
+                }
+            }
+
+            return decoded;
+        }
+
+        public bool IsLeaf(HuffmanNode node)
+        {
+            return (node.LeftNode == null && node.RightNode == null);
+        }
+    }
+}
---
+++ Practica01/Classlibrary/HuffmanNode.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classlibrary
+{
+    public class HuffmanNode
+    {
+        public char Character { get; set; }
+        public int Frequency { get; set; }
+        public HuffmanNode RightNode { get; set; }
+        public HuffmanNode LeftNode { get; set; }
+
+        public List<bool> Traverse(char symbol, List<bool> data)
+        {
+            if(RightNode == null && LeftNode == null)
+            {
+                if(symbol.Equals(this.Character))
+                {
+                    return data;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            else
+            {
+                List<bool> left = null;
+                List<bool> right = null;
+
+                if (LeftNode != null)
+                {
+                    List<bool> leftPath = new List<bool>();
+                    leftPath.AddRange(data);
+                    leftPath.Add(false);
+
+                    left = LeftNode.Traverse(symbol, leftPath);
+                }
+
+                if (RightNode != null)
+                {
+                    List<bool> rightPath = new List<bool>();
+                    rightPath.AddRange(data);
+                    rightPath.Add(true);
+                    right = RightNode.Traverse(symbol, rightPath);
+                }
+
+                if (left != null)
+                {
+                    return left;
+                }
+                else
+                {
+                    return right;
+                }
+            }
+        }
+
+    }
+}
